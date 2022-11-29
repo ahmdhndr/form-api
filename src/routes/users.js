c7bc67392b8979dab2bac46098673be5122fc3d1
erudev <erudev0@gmail.com>
2022-11-29 16:18:15 +0700

    remove jwt middleware where update access token

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,12 +1,11 @@
 import express from 'express';
 import AuthController from '../controllers/AuthController.js';
-import jwtAuth from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 const authController = new AuthController();
 
 router.post('/register', authController.register);
 router.post('/login', authController.login);
-router.put('/login', jwtAuth(), authController.refreshToken);
+router.put('/login', authController.refreshToken);
 
 export default router;
