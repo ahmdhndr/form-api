760b984a1452939d2e7325c1008825100cde432d
erudev <erudev0@gmail.com>
2022-11-29 16:05:48 +0700

    install cors

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import * as dotenv from 'dotenv';
 import express from 'express';
+import cors from 'cors';
 
 import connectDB from './configs/db.js';
 import { errorHandler, notFoundError } from './middlewares/errorMiddleware.js';
@@ -19,6 +20,7 @@ connectDB();
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors({ origin: 'http://localhost:5000' }));
 
 // router
 app.get('/', (req, res) => {
